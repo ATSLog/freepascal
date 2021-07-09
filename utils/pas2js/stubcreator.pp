325b124cb02fa97d04f701ddfda2174158444fae
Henrique Gottardi Werlang <henriquewerlang@hotmail.com>
2021-07-09 11:58:05 -0300

    Parâmetros locais stub.

--- utils/pas2js/stubcreator.pp
+++ utils/pas2js/stubcreator.pp
@@ -369,7 +369,7 @@ begin
   FLineNumberWidth:=4;
   FIndentSize:=2;
   FExtraUnits:='';
-  FOptions:=[woNoImplementation,woNoExternalClass,woNoExternalVar,woNoExternalFunc,woNoAsm,woSkipPrivateExternals,woAlwaysRecordHelper,woSkipHints];
+  FOptions:=[woNoExternalClass,woNoExternalVar,woNoExternalFunc,woNoAsm,woSkipPrivateExternals,woAlwaysRecordHelper,woSkipHints];
 end;
 
 destructor TStubCreator.Destroy;
